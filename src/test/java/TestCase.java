52c621ad35672dbb52894b6dae40cd3db3328343
madaC <madalina.cocian@outlook.com>
2023-08-15 22:29:30 +0300

    Junit 4

---
+++ src/test/java/TestCase.java
@@ -0,0 +1,43 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class TestCase {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        System.out.println("before class");
+    }
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("before");
+    }
+
+    @Test
+    public void testFindMax(){
+        System.out.println("test case find max");
+        assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));
+        assertEquals(0,Calculation.findMax(new int[]{-12,-3,-4,-2}));
+    }
+    @Test
+    public void testCube(){
+        System.out.println("test case cube");
+        assertEquals(27,Calculation.cube(3));
+    }
+    @Test
+    public void testReverseWord(){
+        System.out.println("test case reverse word");
+        assertEquals("ym eman si nahk",Calculation.reverseWord("my name is khan"));
+    }
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("after");
+    }
+
+    @Ignore
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        System.out.println("after class");
+    }
+
+}
